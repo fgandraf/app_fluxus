687962a8aefc27040825a8605fef5240aefef93c
Felipe Gandra <Felipe Gandra@DESKTOP-0AKMI97>
2020-09-30 00:54:03 -0300

    Banco de dado MySQL e RestAPI Deploy no Azure.

--- Fluxus/Model/DAO/Connection.cs
+++ Fluxus/Model/DAO/Connection.cs
@@ -5,8 +5,10 @@ namespace Fluxus.Model.DAO
 {
     public class Connection
     {
-        string dadoscon = "SERVER=localhost; DATABASE=fluxus; UID=root; PWD=; PORT=3308; Sslmode=none; charset=utf8;";
-        //string dadoscon = "SERVER=ayearquitetura.com.br; DATABASE=ayearq02_fluxus; UID=ayearq02_aye; PWD=u1GEnl939ukw;";
+        //private const string _connectionString = "SERVER=localhost; DATABASE=fluxus; UID=root; PWD=; PORT=3308; Sslmode=none; charset=utf8;";
+        //private const string _connectionString = "SERVER=ayearquitetura.com.br; DATABASE=ayearq02_fluxus; UID=ayearq02_aye; PWD=u1GEnl939ukw;";
+        private const string _connectionString = "SERVER=fluxus-server.mysql.database.azure.com; DATABASE=fluxus; UID=fgandraf@fluxus-server; PWD=20pC6iiB0xZI; Sslmode=Required; charset=utf8;";
+
         public MySqlConnection Conn;
 
 
@@ -16,7 +18,7 @@ namespace Fluxus.Model.DAO
         {
             try
             {
-                Conn = new MySqlConnection(dadoscon);
+                Conn = new MySqlConnection(_connectionString);
                 Conn.Open();
             }
             catch (Exception ex)
@@ -29,7 +31,7 @@ namespace Fluxus.Model.DAO
         {
             try
             {
-                Conn = new MySqlConnection(dadoscon);
+                Conn = new MySqlConnection(_connectionString);
                 Conn.Close();
             }
             catch (Exception ex)
