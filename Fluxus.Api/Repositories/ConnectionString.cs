625372a3d142c86081b667b3a23cd808dbfbfd44
fgandraf <fgandraf@gmail.com>
2022-10-24 19:11:13 -0300

    ConnectionString e Dump SQL

---
+++ Fluxus.Api/Repositories/ConnectionString.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace Fluxus.Api.Repositories
+
+{
+    public static class ConnectionString
+    {
+        public const string Conn = "SERVER=localhost:3306; DATABASE=fluxusDB; UID=root;";
+    }
+}
