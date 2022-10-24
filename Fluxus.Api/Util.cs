a1c2b0144d4a58077fea5468df76f334165125cb
fgandraf <fgandraf@gmail.com>
2022-10-24 18:32:57 -0300

    Web Api

---
+++ Fluxus.Api/Util.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fluxus.Api
+{
+    public class Util
+    {
+
+        public static dynamic DateOrNull(string data)
+        {
+            if (data != null)
+                return Convert.ToDateTime(data);
+            else
+                return null;
+        }
+
+        public static string DateTimeToShortDateString(string date)
+        {
+            if (date != "")
+                return Convert.ToDateTime(date).ToString("dd/MM/yyyy");
+            else
+                return null;
+        }
+
+
+
+    }
+}
